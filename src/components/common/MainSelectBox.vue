1e75d57301ddfac4f8c8f2d3f082b1bb8685eed8
Cheonbi <lets.noriter@gmail.com>
2024-11-15 06:40:32 +0900

    "develop:mainview"
    MainView update

---
+++ src/components/common/MainSelectBox.vue
@@ -0,0 +1,75 @@
+<script setup>
+const props = defineProps({
+    placeholder: String,
+})
+</script>
+
+<template>
+    <div class="select-box-inner">
+        <div class="select-box-lbl">
+            <div class="select-box-lbl-inner">
+                <div class="svg-wrap">
+                    <slot name="icon"></slot>
+                </div>
+                <div class="select-input-wrap">
+                    <input name="select" type="text" aria-disabled="false" :maxlength="50" :placeholder="placeholder"
+                        autocomplete="off" autocapitalize="none" autocorrect="off" spellcheck="false"
+                        class="select-input" value="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.svg-wrap {
+    height: 20px;
+    width: 20px;
+    margin-right: 8px;
+}
+
+.svg-fill {
+    fill: rgb(177, 174, 174);
+}
+
+.select-box-inner {
+    position: relative;
+}
+
+.select-box-lbl {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+.select-box-lbl-inner {
+    width: 100%;
+    border-radius: 8px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    position: relative;
+    height: 48px;
+    background-color: rgb(245, 245, 245);
+    padding: 4px 14px;
+}
+
+.select-input-wrap {
+    height: 20px;
+    width: 100%;
+    position: relative;
+}
+
+.select-input {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0);
+    font-size: 1rem;
+    letter-spacing: 0px;
+    line-height: 1.1rem;
+    font-weight: 600;
+    color: rgb(51 51 51);
+    caret-color: rgb(18, 115, 228);
+}
+</style>
---
+++ src/components/icons/IconSearch.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="20" height="20" viewBox="0 0 20 20" sxmlns="http://www.w3.org/2000/svg" class="svg-fill d-block">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M8.85 16.1a6.707 6.707 0 004.394-1.642l-.027.034 3.4 3.508L18 16.574l-3.47-3.58A7.163 7.163 0 0015.7 9.05C15.7 5.156 12.633 2 8.85 2 5.067 2 2 5.156 2 9.05c0 3.894 3.067 7.05 6.85 7.05zm0-1.99c2.695 0 4.88-2.263 4.88-5.055S11.545 4 8.85 4 3.97 6.263 3.97 9.055s2.185 5.055 4.88 5.055z"
+            fill="current"></path>
+    </svg>
+</template>
