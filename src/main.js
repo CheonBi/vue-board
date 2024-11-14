900c5d84f3149e7d2156a0cb60114f53800bf3e0
ChoiJongMin <ocsba0821@naver.com>
2024-11-14 11:52:47 +0900

    vuetify

--- src/main.js
+++ src/main.js
@@ -6,16 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-/**
- * BootStrap Import
- */
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import "@/assets/css/main.css"
 import "bootstrap"
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
